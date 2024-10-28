c8475808f4169755a8a531c8bf084abfe755160a
Atzu Silen <atzusilen@gmail.com>
2024-10-28 09:19:45 +0200

    Bundle after collecting

--- src/@alexi/web/commands/collectstatic.ts
+++ src/@alexi/web/commands/collectstatic.ts
@@ -1,10 +1,11 @@
 import { BaseCommand } from '@alexi/management/base';
-import { collectstatic } from '@alexi/web/server';
+import { bundle, collectstatic } from '@alexi/web/server';
 
 export class Command extends BaseCommand {
   help = 'Collects static files.';
 
   async handle() {
     await collectstatic();
+    await bundle();
   }
 }
