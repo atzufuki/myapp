86822def86d7b5286bd8519147b97bc95354d7ad
Atzu Silen <atzusilen@gmail.com>
2024-11-29 22:18:52 +0200

    Update Alexi

--- src/myapp/static/myapp/app.ts
+++ src/myapp/static/myapp/app.ts
@@ -3,10 +3,9 @@ import { AppConfig } from '@alexi/pwa/config';
 export class MyApp extends AppConfig {
   name = 'myapp';
   getModels = async () => await import(`./models.ts`);
-  getTemplate = async (appName, templateName, extension, context) => {
-    const templates = await import(
-      `./templates/${appName}/${templateName}.${extension}`
-    );
-    return new templates.default(context);
+  getTemplate = async (templateName) => {
+    const [dirName, fileName] = templateName.split('/');
+    const [name, extension] = fileName.split('.');
+    return await import(`./templates/${dirName}/${name}.${extension}`);
   };
 }
